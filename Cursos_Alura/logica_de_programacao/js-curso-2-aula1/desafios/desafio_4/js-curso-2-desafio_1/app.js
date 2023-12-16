4905e0f34fe56a704b06008ffd50af4e9a9e5a46
William Alef <69725099+William-Alef@users.noreply.github.com>
2023-12-16 13:37:45 -0300

    Participando do curso atualizado da Alura

---
+++ Cursos_Alura/logica_de_programacao/js-curso-2-aula1/desafios/desafio_4/js-curso-2-desafio_1/app.js
@@ -0,0 +1,11 @@
+function iniciar(){
+    let listaGenerica = [];
+    let listaTresNomes = ['Maria', 'Paula', 'Roberto'];
+    let linguagensDeProgramacao = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+    linguagensDeProgramacao.push('Java', 'Ruby', 'Python');
+    
+    alert(linguagensDeProgramacao);
+    alert(listaTresNomes[0]);
+    alert(listaTresNomes[1]);
+    alert(listaTresNomes[listaTresNomes.length-1]);
+}
