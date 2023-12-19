6bd442290bff9cda670552b3f3ce4300a2d93612
William Alef <69725099+William-Alef@users.noreply.github.com>
2023-12-19 10:50:18 -0300

    Criando uma lista e percorrendo ela com For e Foreach em busca dos números par.

---
+++ Cursos_Alura/C#/exercicios/forAndForeach/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Criando a lista, percorrendo e verificando se o resto da divisão por 2 é 0, então se o resultado for True, 
+ou seja, se for par será informado ao usuário. Iniciando com o Foreach*/
+using System.ComponentModel;
+using System.Data.Common;
+
+List<int>listaDeInteiros = new List<int>()
+{
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11
+};
+
+foreach (int numero in listaDeInteiros)
+{
+    if(numero %2 == 0)
+    {   
+        Console.Write($"\nLista percorrida pelo Foreach{numero}");
+    };
+}
+
+//Percorrendo a lista com For.
+
+for(int i=0; i < listaDeInteiros.Count; i++)
+{
+    if(listaDeInteiros[i] %2 == 0){
+        Console.Write($"\n\nLista percorrida pelo For{listaDeInteiros[i]}");
+    };
+};
