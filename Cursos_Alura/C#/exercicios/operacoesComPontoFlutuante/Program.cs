6f5800baf1293a7d4491de6f996643b2cb90b0a0
William Alef <69725099+William-Alef@users.noreply.github.com>
2023-12-19 01:08:30 -0300

    Criação de um exercicio que utiliza os conceitos de criação de listas e operações matemáticas.

---
+++ Cursos_Alura/C#/exercicios/operacoesComPontoFlutuante/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Runtime.InteropServices;
+using System.Runtime.Intrinsics.X86;
+using System.Xml.Serialization;
+
+//Solicitando e lendo os números.
+Console.Write("Entre com o primeiro número: ");
+String primeiroNumero = Console.ReadLine()!;
+Console.Write("Entre com o segundo número: ");
+String segundoNumero = Console.ReadLine()!;
+
+//Convertendo a string de entrada em inteiro.
+float a = float.Parse(primeiroNumero);
+float b = float.Parse(segundoNumero);
+
+//Realizando as operações.
+float adicao = a + b;
+float subtracao = a - b;
+float divisao = a / b;
+float multiplicacao = a * b;
+ 
+//Apresentando o resultado ao usuário.
+Console.WriteLine($"\n{a} + {b} = {adicao}\n{a} - {b} = {subtracao}\n{a} / {b} = {divisao}\n{a} * {b} = {multiplicacao}\n");
+
+//Criando uma lista e utilizando foreach para apresentar os itens da lista.
+List<string>bandas = new List<string>()
+{
+    "Iron Maiden",
+    "Metallica",
+    "Megadeth"
+};
+
+foreach (string banda in bandas)
+{
+    Console.WriteLine(banda);
+};
+
+//Criação da lista de números e apresentação para o usuário.
+List<int>listaNumerica = new List<int>()
+{
+    1,
+    2,
+    5,
+    8
+};
+
+int somaNumeros = 0;
+foreach (int numero in listaNumerica)
+{
+    somaNumeros += numero;
+}
+Console.WriteLine($"\nO resultado da soma dos números da lista é {somaNumeros}");
--- Cursos_Alura/C#/exercicios/operacoesComPontoFlutuante/obj/Debug/net8.0/operacoesComPontoFlutuante.AssemblyInfo.cs
+++ Cursos_Alura/C#/exercicios/operacoesComPontoFlutuante/obj/Debug/net8.0/operacoesComPontoFlutuante.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("csharp-projeto-musical")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("operacoesComPontoFlutuante")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("csharp-projeto-musical")]
-[assembly: System.Reflection.AssemblyTitleAttribute("csharp-projeto-musical")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcbd06887b0f05fbc12a9a509cee2b700962ee2d")]
+[assembly: System.Reflection.AssemblyProductAttribute("operacoesComPontoFlutuante")]
+[assembly: System.Reflection.AssemblyTitleAttribute("operacoesComPontoFlutuante")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
