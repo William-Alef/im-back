f329df629d45e080bb7d8ced84ec4dc576b82fc0
William Alef <69725099+William-Alef@users.noreply.github.com>
2023-12-22 15:38:44 -0300

    Percorrendo e apresentando as notas na tela e criando uma função que retorne as opções do menu.

--- Cursos_Alura/C#/exercicios/dicionarioComNotasParaCalculoDaMedia/Program.cs
+++ Cursos_Alura/C#/exercicios/dicionarioComNotasParaCalculoDaMedia/Program.cs
@@ -1,43 +1,81 @@
-﻿Dictionary<string, List<double>> notas = new Dictionary<string, List<double>>();
+﻿using System.ComponentModel.Design;
+using System.Reflection.Metadata;
 
-Console.Write("Qual a contidade de alunos: ");
-int quantidadeAlunos = int.Parse(Console.ReadLine()!);
+Dictionary<string, List<double>> notas = new Dictionary<string, List<double>>();
 
-for (int i = 0; i < quantidadeAlunos; i++)
+opcoesMenu();
+
+void opcoesMenu()
 {
-    Console.Write("Nome do aluno: ");
-    string nome = Console.ReadLine()!;
-    if(notas.ContainsKey(nome))
+    Console.Write("Bem vindo!\n\nDigite o número da ação desejada: \n(1) Para registrar alunos e suas notas.\n(2) Gerenciamento do estoque de uma loja.\n(3) Jogar Quiz.\n(4) Sistema de login.\n\nEscolha:");
+    int escolhaMenu = int.Parse(Console.ReadLine()!);
+
+    switch (escolhaMenu)
     {
-        Console.WriteLine($"O aluno {nome} já consta em sistema, gostaria de adicionar uma nova nota para o mesmo?\nDigite (1) para Sim\n(2) para Não\n");
-        int incluirNota = int.Parse(Console.ReadLine()!);
+        case 1: gerenciamentoDeNotas();
+        break;
+
+        case 2: //gerenciamentoDeEstoque();
+        break;
+
+        case 3: //jogarQuiz();
+        break;
         
-        if(incluirNota == 1)
-        {
-            Console.WriteLine("Quantas notas deseja atribuir ao aluno: ");
-            int quantidadeNotas = int.Parse(Console.ReadLine()!);
+        case 4: //sistemaDeLogin();
+        break;
 
-            for (int ii = 0; ii < quantidadeNotas; ii++)
-            {
-                Console.Write($"Qual a nota do aluno {nome}: ");
-                double novaNota = double.Parse(Console.ReadLine()!);
-                notas[nome].Add(novaNota);
-            }
-        }
-
-    }else
-    {
-        Console.Write($"Qual a nota do aluno {nome}: ");
-        double nota = double.Parse(Console.ReadLine()!);
-        notas.Add(nome, new List<double> {nota});
+        default: Console.WriteLine("\n\nOpção não aceita\nPor favor, Selecione uma opção válida."); Thread.Sleep(2000); opcoesMenu();
+        break;
     }
-    
-}
-/*
-Console.WriteLine("Os alunos e suas notas são:\n");
-for (int i = 0; i < notas.Count; i++)
+};
+
+void gerenciamentoDeNotas()
 {
-    Console.WriteLine($"{notas.Keys.ElementAt(i)} com a nota {notas.Values.ElementAt(i)}");
-    
+    Console.Write("\nQual a quantidade de alunos: ");
+    int quantidadeAlunos = int.Parse(Console.ReadLine()!);
+
+    for (int i = 0; i < quantidadeAlunos; i++)
+    {
+        Console.Write("Nome do aluno: ");
+        string nome = Console.ReadLine()!;
+        if(notas.ContainsKey(nome))
+        {
+            Console.Write($"O aluno {nome} já consta em sistema, gostaria de adicionar uma nova nota para o mesmo?\nDigite:\n\n(1) para Sim\n(2) para Não\nEscolha: ");
+            int incluirNota = int.Parse(Console.ReadLine()!);
+            
+            if(incluirNota == 1)
+            {
+                Console.Write("Quantas notas deseja atribuir ao aluno: ");
+                int quantidadeNotas = int.Parse(Console.ReadLine()!);
+
+                for (int contador = 0; contador < quantidadeNotas; contador++)
+                {
+                    Console.Write($"Qual a nota do aluno {nome}: ");
+                    double novaNota = double.Parse(Console.ReadLine()!);
+                    notas[nome].Add(novaNota);
+                }
+
+                Console.Write("Deseja calcular a média dos alunos: \n(1) Para sim\n(2) Para não\nEscolha: ");
+                int calcularMedia = int.Parse(Console.ReadLine()!);
+
+                if(calcularMedia == 1)
+                {
+                    
+                }
+            }
+        }else
+        {  
+            Console.Write($"Qual a nota do aluno {nome}: ");
+            double nota = double.Parse(Console.ReadLine()!);
+            notas.Add(nome, new List<double> {nota});
+        }
+    }
+    Console.WriteLine("Alunos adicionados: ");
+    foreach (var aluno in notas)
+    {
+        Console.WriteLine($"\n{aluno.Key} com as notas: {string.Join(", ", aluno.Value)}");
+    }  
+    Console.WriteLine("Voltando ao menu inicial.");
+    Thread.Sleep(2000);
+    opcoesMenu();
 }
-*/
--- Cursos_Alura/C#/exercicios/dicionarioComNotasParaCalculoDaMedia/obj/Debug/net8.0/dicionarioComNotasParaCalculoDaMedia.AssemblyInfo.cs
+++ Cursos_Alura/C#/exercicios/dicionarioComNotasParaCalculoDaMedia/obj/Debug/net8.0/dicionarioComNotasParaCalculoDaMedia.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dicionarioComNotasParaCalculoDaMedia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dddc19453f4dbd85c5d3be13994a42837f0434e")]
 [assembly: System.Reflection.AssemblyProductAttribute("dicionarioComNotasParaCalculoDaMedia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dicionarioComNotasParaCalculoDaMedia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
