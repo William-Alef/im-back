fab20a6eb10acf7fef72ade40cd67b10cd407729
William Alef <69725099+William-Alef@users.noreply.github.com>
2025-10-04 03:27:52 -0300

    Exercício: Criação de um sistema para uma oficina
    Conforme proposto no material de estudo, um sistema simples que gerencia uma oficina.

---
+++ Cursos_Alura/C#/exercicios/Oficina-Alura/Oficina-Alura/Oficina.cs
@@ -0,0 +1,45 @@
+﻿namespace Oficina_Alura;
+
+internal class Oficina
+{
+    public List<Veiculo> VeiculosNaOficina { get; set; } = [];
+    public List<Mecanico> Colaboradores { get; set; } = [];
+
+    public Oficina(){}
+
+    public void CadastrarColaborador(Mecanico mecanico)
+    {
+        Colaboradores.Add(mecanico);
+    }
+    public void CadastrarVeiculo(Veiculo veiculo)
+    {
+        VeiculosNaOficina.Add(veiculo);
+    }
+
+    public void AdicionarServico(Mecanico mecanico, Veiculo veiculo)
+    {
+        if(mecanico.AtendendoVeiculo(veiculo))
+        {
+            Console.WriteLine($"Ó veículo já está sendo atendido pelo colaborador {mecanico}");
+        }
+        else if(mecanico == null || veiculo == null)
+        {
+            Console.WriteLine("Mecânico ou veículo inválidos.");
+        }
+        else
+        {
+            mecanico.AtenderCarro(veiculo);
+        }
+    }
+    public void FinalizarServico(Mecanico mecanico, Veiculo veiculo)
+    {
+        mecanico.EntregarServico(veiculo);
+    }
+
+    public override string ToString()
+    {
+        string colaboradoresInfo = String.Join("\n", Colaboradores.Select(c => c.Infos()));
+        return $"No momento temos {VeiculosNaOficina.Count} veículos na oficina." +
+            $"\n\n{colaboradoresInfo}";
+    }
+}
---
+++ Cursos_Alura/C#/exercicios/Oficina-Alura/Oficina-Alura/Program.cs
@@ -0,0 +1,38 @@
+﻿using Oficina_Alura;
+
+namespace AluraOficina;
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Oficina oficina = new();
+        Mecanico joao = new("João", "Mecânica Geral");
+        Mecanico maria = new("Maria", "Elétrica");
+        Mecanico pedro = new("Pedro", "Freios");
+
+        oficina.CadastrarColaborador(joao);
+        oficina.CadastrarColaborador(maria);
+        oficina.CadastrarColaborador(pedro);
+
+        Cliente cliente1 = new("Carlos Garcia", "(11)99999-9999");
+        Cliente cliente2 = new("Gabriel Motta", "(11)99999-9999");
+        Cliente cliente3 = new("Mariana Saal", "(11)99999-9999");
+
+        Veiculo carro1 = new("ABC-1234", "Honda", "Civic", new DateTime(2020), cliente1);
+        Veiculo carro2 = new("DEF-5678", "Fiat", "Palio", new DateTime(2021), cliente2);
+        Veiculo carro3 = new("DEF-5678", "Fiat", "Palio", new DateTime(2021), cliente3);
+        Veiculo carro4 = new("DEF-5678", "Honda", "HB20", new DateTime(2022), cliente3);
+
+        oficina.CadastrarVeiculo(carro1);
+        oficina.CadastrarVeiculo(carro2);
+        oficina.CadastrarVeiculo(carro3);
+        oficina.CadastrarVeiculo(carro4);
+
+        oficina.AdicionarServico(joao, carro1);
+        oficina.AdicionarServico(maria, carro2);
+        oficina.AdicionarServico(maria, carro4);
+        oficina.AdicionarServico(pedro, carro3);
+
+        Console.WriteLine(oficina);
+    }
+}
