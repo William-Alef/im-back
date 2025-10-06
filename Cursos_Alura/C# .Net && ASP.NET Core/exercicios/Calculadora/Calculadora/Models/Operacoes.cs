432a1cfd1052a7e6cf2d8f1f7c77bceacb504263
William Alef <69725099+William-Alef@users.noreply.github.com>
2025-10-05 21:20:35 -0300

    A simple calculator
    A simple calculator that's capable of calculating addition, subtraction, multiplication, division, power and square root.

---
+++ Cursos_Alura/C# .Net && ASP.NET Core/exercicios/Calculadora/Calculadora/Models/Operacoes.cs
@@ -0,0 +1,21 @@
+﻿namespace Calculadora.Models;
+
+internal class Operacoes(double valorA, double ValorB, string operador)
+{
+    public double ValorA { get; set; } = valorA;
+    public double ValorB { get; set; } = ValorB;
+    public string Operador { get; set; } = operador;
+    public double Calculadora()
+    {
+        switch(Operador)
+        {
+            case "+": return ValorA + ValorB;
+            case "-": return ValorA - ValorB;
+            case "*": return ValorA * ValorB;
+            case "/": return ValorB != 0 ? ValorA / ValorB : throw new DivideByZeroException("Não é possível realizar a divisão por zero.");
+            case "**": return Math.Pow(ValorA, ValorB);
+            case "sqr": return Math.Sqrt(ValorA);
+            default: throw new InvalidOperationException("Operador inválido.");
+        }
+    }
+}
---
+++ Cursos_Alura/C# .Net && ASP.NET Core/exercicios/Calculadora/Calculadora/Program.cs
@@ -0,0 +1,83 @@
+﻿using Calculadora.Models;
+using Microsoft.Win32.SafeHandles;
+namespace Calculadora;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        string[] valores = new string[3];
+        while (true)
+        {
+            Menu();
+            valores = Console.ReadLine()!.Split(" ");
+           
+            if (valores.Length == 1 && valores[0].Trim().ToLower() == "sair")
+            {
+                Console.WriteLine("Saindo...");
+                LimparTerminal();
+                break;
+            }
+            if (valores.Length != 3)
+            {
+                Console.WriteLine("\n\nFORMATO INVÁLIDO! Por gentoleza, digite 3 elementos separados por um espaço, conforme o exemplo abaixo:\n 11 + 22\n\n");
+                LimparTerminal();
+                continue;
+            }
+
+            double valorA;
+            double valorB;
+            string operador = valores[1];
+            
+            bool passA = double.TryParse(valores[0], out valorA);
+            bool passB = double.TryParse(valores[2], out valorB);
+            if (!passA || !passB)
+            {
+                Console.WriteLine("\nERRO: Um ou mais valores fornecidos não foram fornecidos corretamente.\n\n");
+                LimparTerminal();
+                continue;
+            }
+
+            try
+            {
+                Operacoes Calculo = new Operacoes(valorA, valorB, operador);
+                Console.WriteLine($"Resultado: {Calculo.Calculadora()}\n\nAperte qualquer tecla para realizar um novo calculo");
+                Console.ReadKey();
+            }
+            catch(InvalidOperationException ex)
+            {
+                Console.WriteLine($"\n\nERRO: Houve um erro de operação. [{ex.Message}]\n\n");
+                LimparTerminal();
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine($"\n\nERRO: Houve um erro de operação [{ex.Message}] \n\n");
+                LimparTerminal();
+            }
+            catch(Exception) { Console.WriteLine("ERRO: Houve um erro desconhecido."); LimparTerminal(); }
+            Console.Clear();
+        }
+        Console.WriteLine("Até a próxima");
+
+        static void LimparTerminal()
+        {
+            Thread.Sleep(1000);
+            Console.Clear();
+        }
+
+        static void Menu()
+        {
+            Console.Write("" +
+                "Operações suportadas com exemplos:" +
+                "\nAdição: 2 + 1." +
+                "\nSubtração: 2 - 1." +
+                "\nMultiplicação: 2 * 1." +
+                "\nDivisão: 2 / 1." +
+                "\nPotenciação: 2 ** 1." +
+                "\nRadiação: 2 sqr 0." +
+                "\nEntre com os valores e o símbolo da operação desejada separados por um espaço, conforme exemplo acima" +
+                "\n\nDigite os valores: "
+            );
+        }
+    }
+}
