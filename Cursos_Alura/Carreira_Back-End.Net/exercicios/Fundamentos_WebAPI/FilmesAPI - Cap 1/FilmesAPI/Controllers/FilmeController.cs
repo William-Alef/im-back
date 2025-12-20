de47695319b5d49cf1f2e0c5b8fc5c8bbef99227
William Alef <69725099+William-Alef@users.noreply.github.com>
2025-12-20 16:13:43 -0300

    Aplicação WEB API em .NET Core
    Estou estudando e criando uma aplicação WEB API do .Net Core, que irá processar informações sobre filmes, incluindo título, gênero e duração.

---
+++ Cursos_Alura/Carreira_Back-End.Net/exercicios/Fundamentos_WebAPI/FilmesAPI - Cap 1/FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,17 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace FilmesAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class FilmeController : ControllerBase
+{
+    private static List<Filme> filmes = [];
+
+    [HttpPost]
+    public void AdicionaFilme([FromBody] Filme filme)
+    {
+        Console.WriteLine($"{filme.Titulo}\n{filme.Genero}\n{filme.Duracao}");
+        filmes.Add(filme);
+    }
+}
---
+++ Cursos_Alura/Carreira_Back-End.Net/exercicios/Fundamentos_WebAPI/FilmesAPI - Cap 1/FilmesAPI/Models/Filme.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
+public class Filme
+{
+    [Required (ErrorMessage = "O filme deve conter um título.")]
+    [MaxLength(200, ErrorMessage = "O título deve conter até 200 caracteres.")]
+    public string Titulo { get; set; }
+
+    [Required (ErrorMessage = "O filme deve conter um gênero.")]
+    [MaxLength(50, ErrorMessage = "O Gênero deve conter até 200 caracteres.")]
+    public string Genero { get; set; }
+
+    [Required (ErrorMessage = "O filme deve conter a sua duração")]
+    [Range (70, 600, ErrorMessage = "O filme deve conter de 70 até 600 minutos de duração.")]
+    public int Duracao { get; set; }
+}
